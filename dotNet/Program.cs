ce2185822aecdc43e7f4d18704d7052d7b92c6c4
Scurlly Rodrigues <92797431+scurlly-rodrigues-kilowott@users.noreply.github.com>
2021-11-29 15:23:30 +0530

    Challenge 4

--- dotNet/Program.cs
+++ dotNet/Program.cs
@@ -9,13 +9,9 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            string projectName = "ACME";
-
-            Console.WriteLine("View English output:\n\t\t" + $@"c:\Exercise\{projectName}\data.txt");
-
-            string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
-            string russianLocation = $@"c:\Exercise\{projectName}\ru-RU\data.txt";
-            Console.WriteLine($"{russianMessage}:\n\t\t{russianLocation}\n");
+            int fahrenheit = 94;
+            decimal celsius = (fahrenheit - 32) * (5m / 9m);
+            Console.WriteLine("The temperature is " + celsius + " Celsius.");
         }
     }
 }
