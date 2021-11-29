3c274562ea21257a1f960bb1875ab662fa0c0a29
Scurlly Rodrigues <92797431+scurlly-rodrigues-kilowott@users.noreply.github.com>
2021-11-29 13:50:48 +0530

    Add project files.

---
+++ dotNet/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("This is the first line.");
+
+Console.Write("This is the second line.");
+
