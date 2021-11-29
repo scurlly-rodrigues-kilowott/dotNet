29522788816385a844d1ff5b33ecec8ba17391da
Scurlly Rodrigues <92797431+scurlly-rodrigues-kilowott@users.noreply.github.com>
2021-11-29 16:22:31 +0530

    Challenge 7

--- dotNet/Program.cs
+++ dotNet/Program.cs
@@ -9,36 +9,14 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            Random random = new Random();
-            int daysUntilExpiration = random.Next(12);
-            int discountPercentage = 0;
+            string[] orderIDs = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
 
-            Console.WriteLine(daysUntilExpiration);
-            if (daysUntilExpiration == 0)
+            foreach (string orderID in orderIDs)
             {
-                Console.WriteLine("Your subscription has expired.");
-
-            }
-            else if (daysUntilExpiration == 1)
-            {
-                discountPercentage = 20;
-                Console.WriteLine("Your subscription expires within a day!Renew now and save " + discountPercentage + "%!");
-
-            }
-            else if (daysUntilExpiration <= 5)
-            {
-                discountPercentage = 10;
-                Console.WriteLine("Your subscription expires in " + daysUntilExpiration + " days.Renew now and save " + discountPercentage + "%!");
-            }
-            else if (daysUntilExpiration <= 10)
-            {
-                Console.WriteLine("Your subscription will expire soon. Renew now!");
-            }
-
-
-            if (daysUntilExpiration > 10)
-            {
-
+                if (orderID.StartsWith("B"))
+                {
+                    Console.WriteLine(orderID);
+                }
             }
 
         }
