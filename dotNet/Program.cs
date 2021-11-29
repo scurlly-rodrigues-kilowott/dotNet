a56974aae7fab19fa5e5831170a74cc9eb75b8eb
Scurlly Rodrigues <92797431+scurlly-rodrigues-kilowott@users.noreply.github.com>
2021-11-29 15:19:42 +0530

    Challenge 3

--- dotNet/Program.cs
+++ dotNet/Program.cs
@@ -9,17 +9,13 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            string name = "Ben";
-            int messages = 3;
-            decimal temperature = 34.4m;
+            string projectName = "ACME";
 
-            Console.Write("Hello, ");
-            Console.Write(name);
-            Console.Write("! You have ");
-            Console.Write(messages);
-            Console.Write(" new messages in your inbox. The temperature is ");
-            Console.Write(temperature);
-            Console.Write(" celsius.");
+            Console.WriteLine("View English output:\n\t\t" + $@"c:\Exercise\{projectName}\data.txt");
+
+            string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
+            string russianLocation = $@"c:\Exercise\{projectName}\ru-RU\data.txt";
+            Console.WriteLine($"{russianMessage}:\n\t\t{russianLocation}\n");
         }
     }
 }
