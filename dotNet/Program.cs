ac736391003358a489085cd7c399c04e232a6a8d
Scurlly Rodrigues <92797431+scurlly-rodrigues-kilowott@users.noreply.github.com>
2021-11-29 16:17:34 +0530

    Challenge 6

--- dotNet/Program.cs
+++ dotNet/Program.cs
@@ -9,11 +9,38 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            int firstValue = 500;
-            int secondValue = 600;
-            int largerValue = Math.Max(firstValue, secondValue);
+            Random random = new Random();
+            int daysUntilExpiration = random.Next(12);
+            int discountPercentage = 0;
 
-            Console.WriteLine("The larger value is " +largerValue);
+            Console.WriteLine(daysUntilExpiration);
+            if (daysUntilExpiration == 0)
+            {
+                Console.WriteLine("Your subscription has expired.");
+
+            }
+            else if (daysUntilExpiration == 1)
+            {
+                discountPercentage = 20;
+                Console.WriteLine("Your subscription expires within a day!Renew now and save " + discountPercentage + "%!");
+
+            }
+            else if (daysUntilExpiration <= 5)
+            {
+                discountPercentage = 10;
+                Console.WriteLine("Your subscription expires in " + daysUntilExpiration + " days.Renew now and save " + discountPercentage + "%!");
+            }
+            else if (daysUntilExpiration <= 10)
+            {
+                Console.WriteLine("Your subscription will expire soon. Renew now!");
+            }
+
+
+            if (daysUntilExpiration > 10)
+            {
+
+            }
+
         }
     }
 }
